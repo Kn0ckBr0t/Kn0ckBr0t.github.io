d5195907507d921b2c001520fbe7ca8e97417270
Pedro Henrique <pedro.ferreira240@etec.sp.gov.br>
2024-10-03 12:59:27 -0300

    Smooth Scroll

--- components/script.js
+++ components/script.js
@@ -1,1 +1,7 @@
 gsap.from(".home__title", { duration: 2.5, opacity: 0, x: '-5%', ease: "expo.out" });
+gsap.registerPlugin(ScrollSmoother);
+ScrollSmoother.create({
+    smooth: 1,
+    effects: true,
+    smoothTouch: 0.1,
+  });
