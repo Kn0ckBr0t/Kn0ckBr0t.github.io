97390af851426663d633ea1ada48decd13cada22
Pedro Henrique <pedro.ferreira240@etec.sp.gov.br>
2024-10-07 18:20:55 -0300

    alterações fundo e home

--- components/script.js
+++ components/script.js
@@ -1,7 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
 gsap.from(".home__title", { duration: 2.5, opacity: 0, x: '-5%', ease: "expo.out" });
-gsap.registerPlugin(ScrollSmoother);
-ScrollSmoother.create({
-    smooth: 1,
-    effects: true,
-    smoothTouch: 0.1,
-  });
+
+gsap.to(".home__sobrenos--WORKWAY", {
+  scale: 1.5,
+  scrollTrigger: {
+    trigger: '.home__sobrenos--WORKWAY',
+    scrub: 1,
+  }
+});
+
+gsap.from(".home__planostitulo--txt", {
+  x: "-10%",
+  opacity: 0,
+  duration: 2,
+  scrollTrigger: '.home__planostitulo--txt',
+});
